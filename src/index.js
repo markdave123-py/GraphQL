3d01f119cd41a8c722f69e4e106bb83f7697c256
markdave123-py <nwaekwudavid@gmail.com>
2023-10-12 16:00:21 +0100

    graphql

--- src/index.js
+++ src/index.js
@@ -27,6 +27,54 @@ const resolvers = {
         author(_, args){
             return db.authors.find((author)=> author.id === args.id)
         }
+    },
+
+    Game: {
+        reviews(parent){
+            return db.reviews.filter((review)=> review.game_id === parent.id)
+        }
+    },
+    Author: {
+        reviews(parent){
+            return db.reviews.filter((review)=> review.author_id === parent.id)
+        }
+    },
+
+    Review: {
+        game(parent){
+            return db.games.find((game)=> game.id === parent.game_id)
+        },
+        author(parent){
+            return db.authors.find((author)=> author.id === parent.author_id)
+        }
+    },
+
+    Mutation: {
+        deleteGame(_, args){
+            const { id } = args
+            db.games = db.games.filter((game)=> game.id !== id)
+            return db.games
+        },
+        addGame(_, args){
+            let  game  = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            }
+            db.games.push(game)
+            return game
+        },
+        updateGame(_, args){
+            db.games = db.games.map((g)=> {
+                if(g.id === args.id){
+                    return {
+                        ...g,
+                        ...args.edits
+                    }
+                }
+                return g
+            })
+            return db.games.find((game)=> game.id === args.id)
+        }
     }
 }
 
--- src/schema.js
+++ src/schema.js
@@ -3,18 +3,22 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
 
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
 
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
 
     type Query{
@@ -24,6 +28,22 @@ export const typeDefs = `#graphql
         author(id: ID!): Author
         games: [Game]
         game(id: ID!): Game
+    }
+
+    type Mutation{
+        addGame(game: GameInput!):Game
+        deleteGame(id: ID!): [Game]
+        updateGame(id: ID!, edits: EditGameInput!): Game 
+    }
+
+    input GameInput {
+        title: String!
+        platform: [String!]!
+    }
+
+    input EditGameInput {
+        title: String
+        platform: [String!]
     }
 
 
